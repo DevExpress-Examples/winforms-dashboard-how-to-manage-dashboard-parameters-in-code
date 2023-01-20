173c83dfaf28cbfff4ac37960f3925e1a75542d1
DevExpressExampleBot <fakeEmail>
2023-01-20 20:14:36 +0400

    Source auto update [skip ci]

--- VB/CustomParametersExample/ViewerForm1.Designer.vb
+++ VB/CustomParametersExample/ViewerForm1.Designer.vb
@@ -1,57 +1,56 @@
-﻿Namespace CustomParametersExample
-	Partial Public Class ViewerForm1
-		''' <summary>
-		''' Required designer variable.
-		''' </summary>
-		Private components As System.ComponentModel.IContainer = Nothing
+Namespace CustomParametersExample
 
-		''' <summary>
-		''' Clean up any resources being used.
-		''' </summary>
-		''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
-		Protected Overrides Sub Dispose(ByVal disposing As Boolean)
-			If disposing AndAlso (components IsNot Nothing) Then
-				components.Dispose()
-			End If
-			MyBase.Dispose(disposing)
-		End Sub
+    Partial Class ViewerForm1
 
-		#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required designer variable.
+        ''' </summary>
+        Private components As System.ComponentModel.IContainer = Nothing
 
-		''' <summary>
-		''' Required method for Designer support - do not modify
-		''' the contents of this method with the code editor.
-		''' </summary>
-		Private Sub InitializeComponent()
-			Me.components = New System.ComponentModel.Container()
-			Me.dashboardViewer = New DevExpress.DashboardWin.DashboardViewer(Me.components)
-			CType(Me.dashboardViewer, System.ComponentModel.ISupportInitialize).BeginInit()
-			Me.SuspendLayout()
-			'
-			' dashboardViewer
-			'
-			Me.dashboardViewer.Dock = System.Windows.Forms.DockStyle.Fill
-			Me.dashboardViewer.Location = New System.Drawing.Point(0, 0)
-			Me.dashboardViewer.Name = "dashboardViewer"
-			Me.dashboardViewer.Size = New System.Drawing.Size(986, 628)
-			Me.dashboardViewer.TabIndex = 0
-			'
-			' ViewerForm
-			'
-			Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
-			Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
-			Me.ClientSize = New System.Drawing.Size(986, 628)
-			Me.Controls.Add(Me.dashboardViewer)
-			Me.Name = "ViewerForm"
-			Me.Text = "Dashboard Viewer"
-			CType(Me.dashboardViewer, System.ComponentModel.ISupportInitialize).EndInit()
-			Me.ResumeLayout(False)
+        ''' <summary>
+        ''' Clean up any resources being used.
+        ''' </summary>
+        ''' <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+            If disposing AndAlso (Me.components IsNot Nothing) Then
+                Me.components.Dispose()
+            End If
 
-		End Sub
+            MyBase.Dispose(disposing)
+        End Sub
 
-		#End Region
+#Region "Windows Form Designer generated code"
+        ''' <summary>
+        ''' Required method for Designer support - do not modify
+        ''' the contents of this method with the code editor.
+        ''' </summary>
+        Private Sub InitializeComponent()
+            Me.components = New System.ComponentModel.Container()
+            Me.dashboardViewer = New DevExpress.DashboardWin.DashboardViewer(Me.components)
+            CType((Me.dashboardViewer), System.ComponentModel.ISupportInitialize).BeginInit()
+            Me.SuspendLayout()
+            '
+            ' dashboardViewer
+            '
+            Me.dashboardViewer.Dock = System.Windows.Forms.DockStyle.Fill
+            Me.dashboardViewer.Location = New System.Drawing.Point(0, 0)
+            Me.dashboardViewer.Name = "dashboardViewer"
+            Me.dashboardViewer.Size = New System.Drawing.Size(986, 628)
+            Me.dashboardViewer.TabIndex = 0
+            '
+            ' ViewerForm
+            '
+            Me.AutoScaleDimensions = New System.Drawing.SizeF(6F, 13F)
+            Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+            Me.ClientSize = New System.Drawing.Size(986, 628)
+            Me.Controls.Add(Me.dashboardViewer)
+            Me.Name = "ViewerForm"
+            Me.Text = "Dashboard Viewer"
+            CType((Me.dashboardViewer), System.ComponentModel.ISupportInitialize).EndInit()
+            Me.ResumeLayout(False)
+        End Sub
 
-		Private dashboardViewer As DevExpress.DashboardWin.DashboardViewer
-	End Class
+#End Region
+        Private dashboardViewer As DevExpress.DashboardWin.DashboardViewer
+    End Class
 End Namespace
-
