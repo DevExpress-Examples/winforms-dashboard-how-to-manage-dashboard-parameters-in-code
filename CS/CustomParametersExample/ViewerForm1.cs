f1d7ca42e66330b865919bd304d03f759ceb985f
Polina Tyureva <polina.tyureva@devexpress.com>
2023-02-06 12:08:32 +0400

    update cs solution

--- CS/CustomParametersExample/ViewerForm1.cs
+++ CS/CustomParametersExample/ViewerForm1.cs
@@ -3,41 +3,35 @@ using System.Linq;
 using System.Collections.Generic;
 using DevExpress.DashboardCommon;
 
-namespace CustomParametersExample
-{
-    public partial class ViewerForm1 : XtraForm
-    {
-        public ViewerForm1()
-        {
+namespace CustomParametersExample {
+    public partial class ViewerForm1 : XtraForm {
+        public ViewerForm1() {
             InitializeComponent();
             dashboardViewer.CustomParameters += DashboardViewer_CustomParameters;
             dashboardViewer.DashboardSource = typeof(SampleDashboard);
-
-            AddParameter(dashboardViewer.Dashboard);
+            dashboardViewer.Dashboard.Parameters.Add(CreateParameter());
             dashboardViewer.Dashboard.DataSources[0].Filter = "[State] In (?parameterState)";
             dashboardViewer.ReloadData();
         }
 
-        private void AddParameter(Dashboard dBoard)
-        {
+        private DashboardParameter CreateParameter() {
             DashboardParameter myDashboardParameter = new DashboardParameter();
             StaticListLookUpSettings staticListLookUpSettings1 = new StaticListLookUpSettings();
             myDashboardParameter.AllowMultiselect = true;
             // Parameter values displayed in the look-up editor.
-            staticListLookUpSettings1.Values = new string[] {"Alabama", "Ohio", "Utah"};
+            staticListLookUpSettings1.Values = new string[] { "Alabama", "Ohio", "Utah" };
             myDashboardParameter.LookUpSettings = staticListLookUpSettings1;
             myDashboardParameter.Name = "parameterState";
             myDashboardParameter.Type = typeof(string);
             // Default parameter value.
             myDashboardParameter.Value = new List<string> { "Ohio", "Utah" };
-            dBoard.Parameters.Add(myDashboardParameter);
+
+            return myDashboardParameter;
         }
 
-        private void DashboardViewer_CustomParameters(object sender, CustomParametersEventArgs e)
-        {
+        private void DashboardViewer_CustomParameters(object sender, CustomParametersEventArgs e) {
             var customParameter = e.Parameters.FirstOrDefault(p => p.Name == "parameterState");
-            if (customParameter != null)
-            {
+            if (customParameter != null) {
                 // Actual value used when retrieving data from the data source.
                 customParameter.Value = "Nevada";
             }
